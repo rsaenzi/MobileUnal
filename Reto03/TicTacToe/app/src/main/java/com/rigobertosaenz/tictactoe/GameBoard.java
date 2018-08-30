661df514a7f95a6a51c971f06db01ee0c37d7d7f
Rigoberto Saenz <beto456789@gmail.com>
2018-08-30 00:33:54 -0500

    Reto3 added

---
+++ Reto03/TicTacToe/app/src/main/java/com/rigobertosaenz/tictactoe/GameBoard.java
@@ -0,0 +1,106 @@
+package com.rigobertosaenz.tictactoe;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class GameBoard extends AppCompatActivity {
+
+    // Represents the internal state of the game
+    private TicTacToeGame mGame;
+
+    // Buttons making up the board
+    private Button mBoardButtons[];
+
+    // Various text displayed
+    private TextView mInfoTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_board);
+
+        mBoardButtons = new Button[TicTacToeGame.BOARD_SIZE];
+        mBoardButtons[0] = (Button) findViewById(R.id.one);
+        mBoardButtons[1] = (Button) findViewById(R.id.two);
+        mBoardButtons[2] = (Button) findViewById(R.id.three);
+        mBoardButtons[3] = (Button) findViewById(R.id.four);
+        mBoardButtons[4] = (Button) findViewById(R.id.five);
+        mBoardButtons[5] = (Button) findViewById(R.id.six);
+        mBoardButtons[6] = (Button) findViewById(R.id.seven);
+        mBoardButtons[7] = (Button) findViewById(R.id.eight);
+        mBoardButtons[8] = (Button) findViewById(R.id.nine);
+
+        mInfoTextView = (TextView) findViewById(R.id.information);
+
+        mGame = new TicTacToeGame();
+        startNewGame();
+    }
+
+    // Set up the game board.
+    private void startNewGame() {
+
+        mGame.clearBoard();
+
+        // Reset all buttons
+        for (int i = 0; i < mBoardButtons.length; i++) {
+            mBoardButtons[i].setText("");
+            mBoardButtons[i].setEnabled(true);
+            mBoardButtons[i].setOnClickListener(new ButtonClickListener(i));
+        }
+
+        // Human goes first
+        mInfoTextView.setText("You go first.");
+
+    }    // End of startNewGame
+
+    private void setMove(char player, int location) {
+
+        mGame.setMove(player, location);
+        mBoardButtons[location].setEnabled(false);
+        mBoardButtons[location].setText(String.valueOf(player));
+        if (player == TicTacToeGame.HUMAN_PLAYER)
+            mBoardButtons[location].setTextColor(Color.rgb(0, 200, 0));
+        else
+            mBoardButtons[location].setTextColor(Color.rgb(200, 0, 0));
+    }
+
+    // Handles clicks on the game board buttons
+    private class ButtonClickListener implements View.OnClickListener {
+
+        int location;
+
+        public ButtonClickListener(int location) {
+            this.location = location;
+        }
+
+        public void onClick(View view) {
+            if (mBoardButtons[location].isEnabled()) {
+                setMove(TicTacToeGame.HUMAN_PLAYER, location);
+
+                // If no winner yet, let the computer make a move
+                int winner = mGame.checkForWinner();
+                if (winner == 0) {
+                    mInfoTextView.setText("It's Android's turn.");
+                    int move = mGame.getComputerMove();
+                    setMove(TicTacToeGame.COMPUTER_PLAYER, move);
+                    winner = mGame.checkForWinner();
+                }
+
+                if (winner == 0)
+                    mInfoTextView.setText("It's your turn.");
+                else if (winner == 1)
+                    mInfoTextView.setText("It's a tie!");
+                else if (winner == 2)
+                    mInfoTextView.setText("You won!");
+                else
+                    mInfoTextView.setText("Android won!");
+            }
+        }
+    }
+
+}
